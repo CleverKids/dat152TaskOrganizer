6c6c02c6937cad0e4ea958a5834ab1061e574bae
Endre <69849510+594630@users.noreply.github.com>
2022-09-21 23:01:26 +0200

    final

--- src/main/webapp/components/tasklist/main.js
+++ src/main/webapp/components/tasklist/main.js
@@ -7,12 +7,11 @@ export default class extends HTMLElement {
     #deletecallbackId = 0;
     #statuses;
 
-    constructor(){
+    constructor() {
         super();
 
-        this.#shadow = this.attachShadow({mode: 'open'});
+        this.#shadow = this.attachShadow({mode: 'closed'});
         this.#createHTML();
-        console.log("hei TaskList");
     }
 
     setDeleteTaskHandler(method) {
@@ -44,29 +43,34 @@ export default class extends HTMLElement {
 
         const content = `
         <H1>Tasks</H1>
-         <p>
+         <p id="waitingMsg">
             Waiting for server data
         </p>
         <button type="button" id="addTaskBtn" disabled>New task</button>
         `
-        wrapper.insertAdjacentHTML('beforeend',content);
+        wrapper.insertAdjacentHTML('beforeend', content);
         this.#shadow.appendChild(wrapper);
 
         return wrapper;
     }
 
-    showTask(newtask){
+    showTask(newtask) {
         const table = this.#shadow.getElementById("tasktable");
+        if (table.hidden === true) {
+            table.hidden = false;
+            this.#shadow.getElementById("noTaskMsg").hidden = true;
+        }
+
         const row = table.insertRow(1);
 
 
         const remove = document.createElement("button");
-        //    `<button id=newtask.id>Remove</button>`;
-            remove.textContent = "Remove";
-            remove.id = newtask.id;
+        remove.textContent = "Remove";
+        remove.id = newtask.id;
+        remove.addEventListener('click', () => {
+            this.deletetaskCallback(newtask.id)
+        });
 
-        //    const tempRemove = this.deletetaskCallback;
-            remove.addEventListener('click',() => {this.deletetaskCallback(newtask.id)});
         const statusOptions = document.createElement("select");
         statusOptions.id = newtask.id;
 
@@ -75,7 +79,7 @@ export default class extends HTMLElement {
         const opt2 = document.createElement("option");
         const opt3 = document.createElement("option");
 
-        opt0.text= "Modify";
+        opt0.text = "Modify";
         opt1.text = this.#statuses[0];
         opt2.text = this.#statuses[1];
         opt3.text = this.#statuses[2];
@@ -84,15 +88,8 @@ export default class extends HTMLElement {
         opt0.hidden = true;
         opt0.selected = true;
 
-        console.log(`${newtask.id}`);
-        console.log(`${this.#statuses[0]}`);
-
         const tempStatuses = this.#statuses;
         const change = this.changestatusCallback;
-
-    //    opt1.addEventListener(`click`, () => this.changestatusCallback(newtask.id,this.#statuses[0]));
-    //    opt2.addEventListener(`click`, () => this.changestatusCallback(newtask.id,this.#statuses[1]));
-    //    opt3.addEventListener(`click`,() => this.changestatusCallback(newtask.id,this.#statuses[2]));
 
         statusOptions.add(opt0);
         statusOptions.add(opt1);
@@ -104,7 +101,7 @@ export default class extends HTMLElement {
                 statusOptions.options[statusOptions.selectedIndex].text));
 
         const idColumn = row.insertCell(0);
-        const titleColumn= row.insertCell(1);
+        const titleColumn = row.insertCell(1);
         const statusColumn = row.insertCell(2);
         const modifyColumn = row.insertCell(3);
         const buttonColumn = row.insertCell(4);
@@ -112,17 +109,19 @@ export default class extends HTMLElement {
         idColumn.textContent = newtask.id;
         idColumn.hidden = true;
         titleColumn.textContent = newtask.title;
-        statusColumn.textContent= newtask.status;
+        statusColumn.textContent = newtask.status;
         modifyColumn.append(statusOptions);
         buttonColumn.append(remove);
     }
 
-    updateTask(status){
+    updateTask(status) {
         const id = status.id;
         const newStatus = status.status;
-        if (table.rows.length > 1){
-            for (var i=1;i<table.rows.length;i++){
-                if (table.rows[i].cells[0].innerHTML == id){
+        const table = this.#shadow.getElementById("tasktable");
+
+        if (table.rows.length > 1) {
+            for (let i = 1; i < table.rows.length; i++) {
+                if (table.rows[i].cells[0].innerHTML == id) {
                     table.rows[i].cells[2].innerHTML = newStatus;
                     break;
                 }
@@ -130,37 +129,39 @@ export default class extends HTMLElement {
         }
     }
 
-    removeTask(id){
+    removeTask(id) {
 
         const table = this.#shadow.getElementById("tasktable");
-        if (table.rows.length > 1){
-            var row_remove = 0,found = 0;
+        if (table.rows.length > 1) {
+            let row_remove = 0, found = 0;
 
-            for (var i=1;i<table.rows.length;i++){
-                if (table.rows[i].cells[0].innerHTML == id){
+            for (let i = 1; i < table.rows.length; i++) {
+                if (table.rows[i].cells[0].innerHTML == id) {
                     row_remove = i;
                     found = 1;
                     break;
                 }
             }
 
-            if(found){
+            if (found) {
                 table.deleteRow(row_remove);
-                console.log('--for testing-- task with id = ['+id+'] was removed');
-            }
-            else{
-                console.log('task with id = ['+id+'] not found');
+                if (table.rows.length < 1) {
+                    noTask();
+                }
             }
         }
     }
 
-    setStatuseslist(statuslist){
+    setStatuseslist(statuslist) {
         this.#statuses = statuslist.allstatuses;
     }
 
-   enableaddtask(){
-       const wrapper = document.createElement('div');
-       const content = `
+    enableaddtask() {
+        let deleteWaitMsg = this.#shadow.getElementById("waitingMsg");
+        deleteWaitMsg.hidden = true;
+
+        const wrapper = document.createElement('div');
+        const content = `
         <form id="taskForm" name = "taskForm">
         <table id = "tasktable">
          <tr>
@@ -170,51 +171,45 @@ export default class extends HTMLElement {
             <th></th>
          </tr>
         </table>
+        <div id="noTaskMsg" hidden><p>No tasks were found.</p></div>
         </form>
         `
-       wrapper.insertAdjacentHTML('beforeend',content);
-       this.#shadow.appendChild(wrapper);
+        wrapper.insertAdjacentHTML('beforeend', content);
+        this.#shadow.appendChild(wrapper);
 
-       const button = this.#shadow.getElementById("addTaskBtn");
-       console.log("heiBox");
-       console.log(button);
-       button.disabled = false;
-       button.addEventListener('click', this.addtaskCallback.bind(this));
-   }
+        const button = this.#shadow.getElementById("addTaskBtn");
+        button.disabled = false;
+        button.addEventListener('click', this.addtaskCallback.bind(this));
+    }
 
-    addtaskCallback(event){
-        console.log("opening taskbox");
+    addtaskCallback(event) {
         event.preventDefault();
         this.message = "";
 
         const met = this.#callbacks.get(this.#addcallbackId);
         met();
-        //    this.#callbacks.forEach(method => { method() });
-
     }
 
-    changestatusCallback(id,newstatus){
-        console.log("status change hei");
+    changestatusCallback(id, newstatus) {
         const newstatusJSON = {};
         newstatusJSON.status = newstatus;
 
         const table = this.#shadow.getElementById("tasktable");
-        if (table.rows.length > 1){
-            var row_find = 0,found = 0;
+        if (table.rows.length > 1) {
+            let row_find = 0, found = 0;
 
-            for (var i=1;i<table.rows.length;i++){
-                if (table.rows[i].cells[0].innerHTML == id){
+            for (let i = 1; i < table.rows.length; i++) {
+                if (table.rows[i].cells[0].innerHTML == id) {
                     row_find = i;
                     found = 1;
                     break;
                 }
             }
 
-            if(found){
-                if(confirm("Set \"" + table.rows[row_find].cells[1].innerHTML + "\" to " + newstatus )) {
+            if (found) {
+                if (confirm("Set \"" + table.rows[row_find].cells[1].innerHTML + "\" to " + newstatus)) {
                     const met = this.#callbacks.get(this.#changecallbackId);
                     met(id, newstatusJSON);
-                    console.log("confirmed");
                 }
             }
         }
@@ -222,38 +217,32 @@ export default class extends HTMLElement {
 
     }
 
-    deletetaskCallback(id){
-        console.log("deleting: ?");
-        console.log("deleting: " + id);
-
+    deletetaskCallback(id) {
         const table = this.#shadow.getElementById("tasktable");
-        if (table.rows.length > 1){
-            var row_find = 0,found = 0;
+        if (table.rows.length > 1) {
+            let row_find = 0, found = 0;
 
-            for (var i=1;i<table.rows.length;i++){
-                if (table.rows[i].cells[0].innerHTML == id){
+            for (let i = 1; i < table.rows.length; i++) {
+                if (table.rows[i].cells[0].innerHTML == id) {
                     row_find = i;
                     found = 1;
                     break;
                 }
             }
 
-            if(found){
-                if(confirm("Delete task \'" + table.rows[row_find].cells[1].innerHTML + "\'?")) {
+            if (found) {
+                if (confirm("Delete task \'" + table.rows[row_find].cells[1].innerHTML + "\'?")) {
                     const met = this.#callbacks.get(this.#deletecallbackId);
                     met(id);
-                    console.log("confirmed");
                 }
             }
         }
-
     }
 
     noTask() {
+        const table = this.#shadow.getElementById("tasktable");
+        table.hidden = true;
 
-        const form = this.#shadow.getElementById("tasktable");
-        form.parentNode.removeChild(form);
-
-        this.#shadow.getElementById("taskForm").innerHTML = '<p>No tasks were found.</p>';
+        this.#shadow.getElementById("noTaskMsg").hidden = false;
     }
 }
--- src/main/webapp/js/taskview.js
+++ src/main/webapp/js/taskview.js
@@ -1,126 +1,123 @@
 import TaskList from '../components/tasklist/main.js';
 import TaskBox from '../components/taskbox/main.js';
+
 customElements.define('task-list', TaskList);
 customElements.define('task-box', TaskBox);
 
-console.log("heiTasksview1");
+const url = "../TaskServices/api/services";
 
-        const url = "../TaskServices/api/services";
+const taskbox = document.querySelector("task-box");
+const tasklist = document.querySelector("task-list");
+const statuslistResponse = await fetch(url + "/allstatuses", {method: "GET"});
+const statuslistResult = await statuslistResponse.json();
 
-        const taskbox = document.querySelector("task-box");
-        const tasklist = document.querySelector("task-list");
-        console.log("box:" + taskbox);
-        const statuslistResponse = await fetch(url + "/allstatuses", {method: "GET"});
-        const statuslistResult = await statuslistResponse.json();
+taskbox.setStatuseslist(statuslistResult);
+tasklist.setStatuseslist(statuslistResult);
 
-        taskbox.setStatuseslist(statuslistResult);
-        tasklist.setStatuseslist(statuslistResult);
+tasklist.enableaddtask();
+taskbox.setAddTaskHandler(addTask.bind(this));
+tasklist.setDeleteTaskHandler(deleteTask.bind(this));
+tasklist.setNewTaskHandler(showTaskBox.bind(this));
+tasklist.setUpdateTaskHandler(updateTask.bind(this));
 
-        tasklist.enableaddtask();
-        taskbox.setAddTaskHandler(addTask.bind(this));
-        tasklist.setDeleteTaskHandler(deleteTask.bind(this));
-        tasklist.setNewTaskHandler(showTaskBox.bind(this));
-        tasklist.setUpdateTaskHandler(updateTask.bind(this));
+const tasklistResponse = await fetch(url + "/tasklist", {method: "GET"});
+const tasklistResult = await tasklistResponse.json();
 
-        const tasklistResponse = await fetch(url + "/tasklist", {method: "GET"});
-        const tasklistResult = await tasklistResponse.json();
+if (tasklistResult.responseStatus === true) {
+    if (tasklistResult.tasks.length > 0) {
+        tasklistResult.tasks.forEach(t => tasklist.showTask(t));
+    } else {
+        tasklist.noTask();
+    }
+}
 
-        if(tasklistResult.responseStatus === true) {
-            tasklistResult.tasks.forEach(t => tasklist.showTask(t));
-        }
-
-function showTaskBox(){
+function showTaskBox() {
     taskbox.show();
 }
 
-        async function addTask(task) {
-        const requestSettings = {
-            "method": "POST",
-            "headers": { "Content-Type": "application/json; charset=utf-8" },
-            "body": JSON.stringify(task),
-            "cache": "no-cache",
-            "redirect": "error"
-        };
+async function addTask(task) {
+    const requestSettings = {
+        "method": "POST",
+        "headers": {"Content-Type": "application/json; charset=utf-8"},
+        "body": JSON.stringify(task),
+        "cache": "no-cache",
+        "redirect": "error"
+    };
 
-        try {
-            const response = await fetch(url+"/task", requestSettings);
-            if (response.ok) {
-                const object = await response.json();
+    try {
+        const response = await fetch(url + "/task", requestSettings);
+        if (response.ok) {
+            const object = await response.json();
 
-                if (typeof object.responseStatus != "undefined") {
-                    if (object.responseStatus) {
-                        taskbox.close();
-                        tasklist.showTask(object.task);
-                    } else {
-                        console.log("Could not connect to server");
-                    }
+            if (typeof object.responseStatus != "undefined") {
+                if (object.responseStatus) {
+                    taskbox.close();
+                    tasklist.showTask(object.task);
                 } else {
                     console.log("Could not connect to server");
                 }
+            } else {
+                console.log("Could not connect to server");
             }
-        } catch (e) {
-            console.log("Could not connect to server");
         }
+    } catch (e) {
+        console.log("Could not connect to server");
     }
+}
 
-    async function updateTask(taskId,newStatus) {
-        const requestSettings = {
-            "method": "PUT",
-            "headers": {"Content-Type": "application/json; charset=utf-8"},
-            "body": JSON.stringify(newStatus),
-            "cache": "no-cache",
-            "redirect": "error"
-        };
+async function updateTask(taskId, newStatus) {
+    const requestSettings = {
+        "method": "PUT",
+        "headers": {"Content-Type": "application/json; charset=utf-8"},
+        "body": JSON.stringify(newStatus),
+        "cache": "no-cache",
+        "redirect": "error"
+    };
 
-        try {
-            const response = await fetch(url+"/task/" + taskId, requestSettings);
-            if (response.ok) {
-                const object = await response.json();
+    try {
+        const response = await fetch(url + "/task/" + taskId, requestSettings);
+        if (response.ok) {
+            const object = await response.json();
 
-                if (typeof object.responseStatus != "undefined") {
-                    if (object.responseStatus) {
-                       tasklist.updateTask(object);
-                    } else {
-                        console.log("Could not connect to server");
-                    }
+            if (typeof object.responseStatus != "undefined") {
+                if (object.responseStatus) {
+                    tasklist.updateTask(object);
                 } else {
                     console.log("Could not connect to server");
                 }
+            } else {
+                console.log("Could not connect to server");
             }
-        } catch (e) {
-            console.log("Could not connect to server");
         }
+    } catch (e) {
+        console.log("Could not connect to server");
     }
+}
 
-    async function deleteTask(taskId)
-
-    {
-        console.log("about to delete:" + taskId + "!");
-
-        const requestSettings = {
-            "method": 'DELETE',
-            "headers": {"Content-Type": "application/json; charset=utf-8"},
-            "body": "",
-            "cache": "no-cache",
-            "redirect": "error"
-        };
-        try {
-            const response = await fetch(url+"/task/" + taskId, requestSettings);
-            if (response.ok) {
-                const object = await response.json();
-                if (typeof object.responseStatus != "undefined") {
-                    if (object.responseStatus) {
-                        tasklist.removeTask(object.id);
-                        console.log("deleted task: " + taskId);
-                    } else {
-                        console.log("Could not connect to server");
-                    }
+async function deleteTask(taskId) {
+    const requestSettings = {
+        "method": 'DELETE',
+        "headers": {"Content-Type": "application/json; charset=utf-8"},
+        "body": "",
+        "cache": "no-cache",
+        "redirect": "error"
+    };
+    try {
+        const response = await fetch(url + "/task/" + taskId, requestSettings);
+        if (response.ok) {
+            const object = await response.json();
+            if (typeof object.responseStatus != "undefined") {
+                if (object.responseStatus) {
+                    tasklist.removeTask(object.id);
                 } else {
                     console.log("Could not connect to server");
                 }
+            } else {
+                console.log("Could not connect to server");
             }
-        } catch (e) {
-            console.log("Could not connect to server");
         }
+    } catch (e) {
+        console.log("Could not connect to server");
+    }
 
 }
